5050ec8aeae5fd7256882bf7c155de8bc29ba290
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2020-08-25 13:59:45 +0200

    hello Niklas

---
+++ fredik/fredik/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fredik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Niklas!");
+        }
+    }
+}
