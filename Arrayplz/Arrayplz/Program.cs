fee4c13e523b4dd3a5d25a267a78d066e367357a
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2020-09-08 14:14:53 +0200

    ja

---
+++ Arrayplz/Arrayplz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arrayplz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Name = new String[5];
+
+            Console.WriteLine("Skriv in 5 nama");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Name[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Du skrev in namnen");
+
+
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine(Name[j]);
+
+            }
+        }
+    }
+}
