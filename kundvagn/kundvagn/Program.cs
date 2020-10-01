a838adf2b7cae6530b8938746ecbd8ed1da365f8
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2020-10-01 15:48:52 +0200

    h

--- kundvagn/kundvagn/Program.cs
+++ kundvagn/kundvagn/Program.cs
@@ -16,20 +16,22 @@ namespace kundvagn
 
             kund._name = Console.ReadLine(); // nager din kund
 
-            Console.WriteLine("hej"+ kund._name);
+            Console.WriteLine("hej "+ kund._name);
 
             List<string> kunder = new List<string>();
 
-            Console.WriteLine("Om du vill logga in som en annan kund tryck 1");
-            Console.WriteLine("Om du vill köppa våra varor tryck 2");
-            Console.WriteLine("Om du vill kolla din kuundvagn tryck 3");
-            Console.WriteLine("Om du vill avsluta din köp tryck 4");
+           
 
             bool loop = false;
 
             while (!loop)
             {
                 int meny = 1;
+                
+                Console.WriteLine("Om du vill logga in som en annan kund tryck 1");
+                Console.WriteLine("Om du vill köppa våra varor tryck 2");
+                Console.WriteLine("Om du vill kolla din kuundvagn tryck 3");
+                Console.WriteLine("Om du vill avsluta din köp tryck 4");
 
                 meny = Convert.ToInt32(Console.ReadLine());
 
@@ -40,25 +42,33 @@ namespace kundvagn
                         kunder.Add(kund._name);
 
                         skapakunder(kunder);
-                        //skapa ny kund
+
                         
-                        for (int i = 0; i < kunder.Count; i++)
-                        {
-                            Console.WriteLine(kunder[i]);
+                        //skapa ny kund
 
-                        }
-
+                        
                         break;
                     case 2:
 
-                        Console.WriteLine(köpvaror(kund._snabmat));
-
-                        
+                        köpvaror(kund._snabmat);
 
                         // köp vara till kund
 
                         break;
                     case 3:
+
+                        for (int i = 0; i < kunder.Count; i++)
+                        {
+                            Console.WriteLine(kunder[i] + " Har köpt");
+
+                            for (int j = 0; j < kund._snabmat.Count; j++)
+                            {
+                                Console.WriteLine(kund._snabmat[j]._snabmatnamn);
+
+                            }
+
+                        }
+
 
                         //kola ina varoe
 
@@ -94,9 +104,76 @@ namespace kundvagn
         public static List<Produkt> köpvaror(List<Produkt> produkter)
         {
             Produkt snabmat = new Produkt();
-            snabmat._snabmatnamn = "burjare";
+
+            int meny = 1;
+
+            Console.WriteLine("vill du köpa haburgare tryck 1");
+            Console.WriteLine("vill du köpa korv med bröd tryck 2");
+            Console.WriteLine("vill du köpa coca cola tryck 3");
+
+            meny = Convert.ToInt32(Console.ReadLine());
+
+            switch (meny)
+            {
+
+                case 1:
+                    Console.WriteLine("hur många vill du ha?");
+
+                    int S = 0;
+                    S = Convert.ToInt32(Console.ReadLine());
+
+                    for (int i = 0; i < S; i++)
+                    {
+                    snabmat._snabmatnamn = "burgare";
+
+                        produkter.Add(snabmat);
+                    }
+
+                    for (int i = 0; i < produkter.Count; i++)
+                    {
+                        Console.WriteLine(produkter[i]._snabmatnamn);
+
+                    }
+
+
+                    break;
+                case 2:
+                    Console.WriteLine("hur många vill du ha?");
+
+                    int K = 0;
+                    K = Convert.ToInt32(Console.ReadLine());
+
+                    for (int i = 0; i < K; i++)
+                    {
+                        snabmat._snabmatnamn = "korv med bröd";
+
+                        produkter.Add(snabmat);
+                    }
+
+
+                    break;
+                case 3:
+                    Console.WriteLine("hur många vill du ha?");
+
+                    int C = 0;
+                    C = Convert.ToInt32(Console.ReadLine());
+
+                    for (int i = 0; i < C; i++)
+                    {
+                        snabmat._snabmatnamn = "coca cola";
+
+                        produkter.Add(snabmat);
+                    }
+
+                    
+
+                    break;
+
+                default:
+
+                    break;
+            }
             
-            produkter.Add(snabmat);
 
             return produkter;
 
