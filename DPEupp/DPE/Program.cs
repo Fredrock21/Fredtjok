1d674755d3eb4ebb80287a98214426f273c151f8
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2021-01-18 12:10:31 +0100

    ja

---
+++ DPEupp/DPE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DPE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
