552e03bca3c6ac0df25e1c1614b9c7846f28930f
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2021-02-01 12:41:36 +0100

    börjar

---
+++ labbSP/labbSP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace labbSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the pet stor");
+            Console.WriteLine("You kan by a katt or a dog");
+
+            String Pname = Console.ReadLine();
+
+            if(Pname = "katt" && "dog")
+
+
+            bool reset = false;
+
+
+            Console.WriteLine("vat du you wont to do with the Pname");
+
+            switch (operation)
+            {
+                case "+":
+                    
+                    break;
+                case "-":
+                    
+                    break;
+                case "*":
+                    
+                    break;
+                case "/":
+                   
+                    break;
+                default:
+                    Console.WriteLine("Please select a valid operation");
+                    reset = true;
+                    
+                    break;
+            }
+
+
+
+
+        }
+    }
+}
---
+++ labbSP/labbSP/katt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace labbSP
+{
+    class katt : pet
+    {
+    }
+}
