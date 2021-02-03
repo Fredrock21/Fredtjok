de49a7d615f0dba792919f935dc9c37d44da5381
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2021-02-03 13:44:28 +0100

    hmmm

--- labbSP/labbSP/Program.cs
+++ labbSP/labbSP/Program.cs
@@ -7,17 +7,31 @@ namespace labbSP
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the pet stor");
+            
+
+            bool P = false;
+
             Console.WriteLine("You kan by a katt or a dog");
+            
+            String chose = Console.ReadLine();
 
-            String Pname = Console.ReadLine();
+            if (chose = "katt" && "dog")
+            {
+                P = true;
+            }
+            else
+            {
+                Console.WriteLine("You didet chos a katt or a dog");
+            }
 
-            if(Pname = "katt" && "dog")
 
 
             bool reset = false;
 
 
-            Console.WriteLine("vat du you wont to do with the Pname");
+            Console.WriteLine("what du you wont to do with the" + chose);
+
+            var operation = Console.ReadLine();
 
             switch (operation)
             {
