6fa45352b776e6424094845a7a69b6d0e7cd8f90
Fredrock21 <54800949+Fredrock21@users.noreply.github.com>
2021-03-16 13:52:14 +0100

    hum

---
+++ Newspaper/Newspaper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Newspaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Newspaper/Newspaper/Readers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Newspaper
+{
+    class Reader
+    {
+
+        public Reader name { get; set };
+
+
+
+    }
+}
